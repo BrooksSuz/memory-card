013bb5aea659e61ce838a36d202604c9647dad95
Brooks Susor <susorbrooks@gmail.com>
2022-11-12 11:04:24 -0500

    Fix values in indidvidual Cards

--- src/App.js
+++ src/App.js
@@ -2,29 +2,30 @@ import React from "react";
 import Card from "./components/card";
 
 const App = () => {
-  <div className="container">
+  return (
+    <div className="container">
 
-    <header>
-      <h1>Memory Card Game</h1>
-    </header>
+      <header>
+        <h1>Memory Card Game</h1>
+      </header>
 
-    <div className="card-grid">
-      <Card letter="a" />
-      <Card letter="b" />
-      <Card letter="c" />
-      <Card letter="d" />
-      <Card letter="e" />
-      <Card letter="f" />
-      <Card letter="g" />
-      <Card letter="h" />
-      <Card letter="i" />
-      <Card letter="j" />
-      <Card letter="k" />
-      <Card letter="l" />
+      <div className="card-grid">
+        <Card letter="a" />
+        <Card letter="b" />
+        <Card letter="c" />
+        <Card letter="d" />
+        <Card letter="e" />
+        <Card letter="f" />
+        <Card letter="a" />
+        <Card letter="b" />
+        <Card letter="c" />
+        <Card letter="d" />
+        <Card letter="e" />
+        <Card letter="f" /> 
+      </div>
 
     </div>
-
-  </div>
+  );
 };
 
 export default App;
--- src/components/card.js
+++ src/components/card.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Card = (props) => {
-  
+  const { letter } = props;
+
+  return (
+    <div className="card">
+      <div className="letter">{letter}</div>
+    </div>
+  );
 };
 
 export default Card;
